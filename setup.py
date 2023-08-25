dcd89c670b5529cffd8eb899ffc085b09dba2d3a
BADAVATH SRIKANTH <80474350+SRIKANTH284@users.noreply.github.com>
2023-08-25 14:13:45 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
 name='mlproject',
 version='0.0.1',
-author='Krish',
-author_email='krishnaik06@gmail.com',
+author='Srikanth',
+author_email='badavathsrikanth149@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 
