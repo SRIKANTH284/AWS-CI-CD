7076b8c2af1e6c46e9d7d7741e5f02237bc8792b
BADAVATH SRIKANTH <80474350+SRIKANTH284@users.noreply.github.com>
2023-08-25 12:48:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
 
-
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
